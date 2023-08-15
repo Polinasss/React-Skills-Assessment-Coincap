bd1831a5099267ab76259b92e9a66386472b2c53
Polinasss <polina129889@gmail.com>
2023-08-15 12:18:25 +0300

    fix: remove gh-pages (error)

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-export const fetchCoincapApi = async (id: string) => {
+export const fetchCoincapApi = async (id: string = '') => {
       const response = await fetch(`https://api.coincap.io/v2/assets/${id}`);
       if(!response) {
         throw new Response('', {status: 404, statusText: 'Error'})
