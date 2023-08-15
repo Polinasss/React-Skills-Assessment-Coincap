bc3507d8fa72f94ae7f4d13470199171131375d0
Polinasss <polina129889@gmail.com>
2023-08-15 12:32:07 +0300

    fix: fix services

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,11 @@
 export const fetchCoincapApi = async (id: string = '') => {
-      const response = await fetch(`https://api.coincap.io/v2/assets/${id}`);
+  try {
+    const response = await fetch(`https://api.coincap.io/v2/assets/${id}`);
       if(!response) {
         throw new Response('', {status: 404, statusText: 'Error'})
       }
       return await response.json();
+  } catch (error) {
+    console.log(error)
+  }
   }
